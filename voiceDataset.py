da2351417a90bbad24a31d53e5bad6d12965f08f
wyyadd <wyyadd@gmail.com>
2021-10-29 20:31:29 +0800

    train test

--- voiceDataset.py
+++ voiceDataset.py
@@ -2,16 +2,17 @@ import os
 from typing import Tuple
 import torch
 import torchaudio
-from torch import Tensor, nn
+from torch import Tensor
 from torch.utils.data import Dataset
 from encodeAndDecode import Encode
+import librosa
+import numpy
 
 
 def load_voice_item(filename: str,
-                    path: str) -> Tuple[Tensor, int, Tensor]:
+                    path: str) -> Tuple[numpy.ndarray, int, Tensor]:
     filename = path + "/data/" + filename
-    waveform, sample_rate = torchaudio.load(filename)
-    waveform = torch.reshape(waveform, (-1,))
+    waveform, sample_rate = librosa.load(filename)
     label = []
     with open(filename + ".trn", encoding="utf8") as labels:
         for line in labels:
@@ -28,14 +29,13 @@ def pad_collate(batch):
     input_lengths = []
     label_lengths = []
     for waveform, sample_rate, label in batch:
-        mfcc_transform = torchaudio.transforms.MFCC(
-            sample_rate=sample_rate,
-            n_mfcc=40
-        )
         label_lengths.append(len(label))
         labels.append(label)
-        mfcc.append(mfcc_transform(waveform).transpose(0, 1))
-        input_lengths.append(mfcc[-1].shape[0] // 2)
+        # feature, time
+        feature = librosa.feature.mfcc(waveform, sample_rate, n_mfcc=40)
+        # time, feature
+        mfcc.append(torch.from_numpy(feature).transpose(0, 1))
+        input_lengths.append(len(feature[1]) // 2)
     mfcc = torch.nn.utils.rnn.pad_sequence(mfcc, batch_first=True).unsqueeze(1).transpose(2, 3)
     labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True)
     return mfcc, labels, input_lengths, label_lengths
@@ -49,7 +49,7 @@ class VoiceDataset(Dataset):
             if os.path.splitext(name)[1] == ".wav":
                 self._walker.append(name)
 
-    def __getitem__(self, idx) -> Tuple[Tensor, int, Tensor]:
+    def __getitem__(self, idx) -> Tuple[numpy.ndarray, int, Tensor]:
         filename = self._walker[idx]
         return load_voice_item(filename, self._dir)
 
